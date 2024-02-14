a093db41f3b396dfe66afed4e9ee8a4f3fca4855
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2024-02-14 12:18:42 +0100

    Updated playermovement to use unity events and a more customizable version

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using FrameWork.Extensions;
 using FrameWork.GridSystem;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace Player.Movement
 {
     public class PlayerMovement : MonoBehaviour
-    { 
-        [SerializeField, Range(1,10)] private float movementSpeed;
+    {
+        
+        [SerializeField, Range(1,10)] private float movementSpeed; 
+        [SerializeField] private UnityEvent startedMoving = new();
         
         private int _enumLength;
         private int _gridIndex;
@@ -17,15 +21,13 @@ namespace Player.Movement
 
         private void Awake()=> _enumLength = Enum.GetNames(typeof(PlayerGridPoints)).Length;
         
-        private IEnumerator MoveTowardsGridPoint()
+        private IEnumerator MoveTowardsGridPoint(PlayerGridPoints points)
         {
             if (!_moving)
             {
-                _gridIndex = (_gridIndex + 1) % _enumLength;
-                PlayerGridPoints nextpoint = (PlayerGridPoints) _gridIndex;
-
-                Vector3 newPos;
-                newPos = nextpoint.GetVector3();
+                PlayerGridPoints playerGridPoint = points;
+                
+                Vector3 newPos = playerGridPoint.GetVector3();
                 while (transform.position != newPos)
                 {
                     _moving = true;
@@ -39,6 +41,25 @@ namespace Player.Movement
         /// <summary>
         /// Starts the coroutine to start moving the player from point A > The next point
         /// </summary>
-        public void StartMoving() => StartCoroutine(MoveTowardsGridPoint());
+        public void StartMoving(int playerPoints)
+        {
+            PlayerGridPoints points = (PlayerGridPoints)playerPoints;
+            switch (points)
+            {
+                case PlayerGridPoints.POINT_A:
+                    StartCoroutine(MoveTowardsGridPoint(PlayerGridPoints.POINT_A));
+                    break;
+                case PlayerGridPoints.POINT_B:
+                    StartCoroutine(MoveTowardsGridPoint(PlayerGridPoints.POINT_B));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Starts a unity event to call the coroutine for moving the player. This allows for us to choose which point the player moves with unity events
+        /// </summary>
+        public void StartMovingEvent() => startedMoving.Invoke();
     }
 }
