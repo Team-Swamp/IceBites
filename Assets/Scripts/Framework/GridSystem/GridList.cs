4b93d40e0d3605341ac1ec589d7580fb07cd7397
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2024-02-14 12:01:58 +0100

    Updated GridList to add NPC enums

--- Assets/Scripts/Framework/GridSystem/GridList.cs
+++ Assets/Scripts/Framework/GridSystem/GridList.cs
@@ -3,9 +3,15 @@ using FrameWork.Extensions;
 
 namespace FrameWork.GridSystem
 {
-    public enum GridPoints
+    public enum PlayerGridPoints
     {
-        [Vector3Value(1,1,1)] POINT_A,
+        [Vector3Value(0,1,0)] POINT_A,
         [Vector3Value(5,1,5)] POINT_B,
+    }
+
+    public enum NpcGridpoints
+    {
+        [Vector3Value(0,1,-15)] STARTING_POINT,
+        [Vector3Value(0,1,-3)] COUNTER_POINT,
     }
 }
