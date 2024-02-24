a9724e0774ed950354e6a0923aa399598a6badfd
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2024-02-25 00:02:21 +0100

    Abstract movement class

---
+++ Assets/Scripts/Framework/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using FrameWork.Extensions;
+using UnityEngine;
+namespace Framework
+{
+    public abstract class Movement : MonoBehaviour
+    {
+        [Range(1,10)] private float _movementSpeed; 
+
+        private bool _moving;
+        public float MovementSpeed
+        {
+            get => _movementSpeed;
+            set => _movementSpeed = value;
+        }
+
+        protected IEnumerator MoveTowardsGridPoint<T>(T points) where T: Enum
+        {
+            if (!_moving)
+            {
+                
+                Vector3 newPos = points.GetVector3();
+                while (transform.position != newPos)
+                {
+                    _moving = true;
+                    transform.position = Vector3.MoveTowards(transform.position, newPos, MovementSpeed * Time.deltaTime);
+                    yield return null;
+                }
+
+                _moving = false;
+            }
+        } 
+    }
+}
