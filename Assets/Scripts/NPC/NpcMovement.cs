b7f853d7e1894a270988446f1e232af664dab98e
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2024-02-26 10:48:03 +0100

    Update NpcMovement.cs

--- Assets/Scripts/NPC/NpcMovement.cs
+++ Assets/Scripts/NPC/NpcMovement.cs
@@ -10,29 +10,13 @@ namespace NPC
     {
         private void Start() => StartCoroutine(MovingNpc());
         
-        public void MoveNpc(int points)
-        {
-            NpcPoints npcPoints = (NpcPoints)points;
-            switch (npcPoints)
-            {
-                case NpcPoints.NPC_STARTING_POINT:
-                    StartCoroutine(MoveTowardsGridPoint(NpcPoints.NPC_STARTING_POINT));
-                    break;
-                case NpcPoints.NPC_COUNTER_POINT:
-                    StartCoroutine(MoveTowardsGridPoint(NpcPoints.NPC_COUNTER_POINT));
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-        
         private IEnumerator MovingNpc()
         {
             while (true)
             {
-                MoveNpc(1);
+                StartCoroutine(MoveTowardsGridPoint(NpcPoints.NPC_STARTING_POINT));
                 yield return new WaitForSeconds(1);
-                MoveNpc(0);
+                StartCoroutine(MoveTowardsGridPoint(NpcPoints.NPC_COUNTER_POINT));
             }
         }
     }
